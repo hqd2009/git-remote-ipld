43fdcc7dde12543eda953e2d1549b2e89059ad34
hqd <hqd@QingdaHuMacBook-Pro-2.local>
2017-10-08 22:52:19 +0800

    sync with badget-updates

--- tracker.go
+++ tracker.go
@@ -34,13 +34,11 @@ func NewTracker(gitPath string) (*Tracker, error) {
 
 func (t *Tracker) GetRef(refName string) ([]byte, error) {
 	txn := t.kv.NewTransaction(true)
-	var it badger.Item
-	var err error
-	&it, err = txn.Get([]byte(refName))
+	it, err := txn.Get([]byte(refName))
 	if err != nil {
 		return nil, err
 	}
-	return syncBytes(it.Value)
+	return syncBytes((*it).Value)
 }
 
 func (t *Tracker) SetRef(refName string, hash []byte) error {
